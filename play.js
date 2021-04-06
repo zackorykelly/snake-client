f7c29684f4238672d9c142ee851ae90340705d6a
Zackory Kelly <zackkelly@live.ca>
2021-04-06 23:54:18 +0000

    Added user input detection to play.js

--- play.js
+++ play.js
@@ -3,3 +3,22 @@ const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
 
+/**
+ * Setup User Interfac
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.stdout.write('Thanks for using me, ciao!\n');
+    process.exit();
+  }
+};
