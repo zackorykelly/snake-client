0b7ed10ced39c117445a97915f237448b9772178
Zackory Kelly <zackkelly@live.ca>
2021-04-06 23:20:01 +0000

    Added connected message and sending of initials to server

--- client.js
+++ client.js
@@ -15,9 +15,14 @@ const connect = function() {
     console.log('Server says: ', data);
   });
 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: ZNK');
+  });
+
   return conn;
-}
+};
 
 module.exports = {
   connect
-}
+};
