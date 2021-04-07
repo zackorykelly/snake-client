e3b0b1815a3558bd337722038fc3ea4aa129ee1b
Zackory Kelly <zackkelly@live.ca>
2021-04-07 15:00:38 +0000

    Added movement functionality by sending input to server

--- input.js
+++ input.js
@@ -1,8 +1,12 @@
+// Stores the active TCP connection object.
+let connection;
+
 /**
  * Setup User Interfac
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -14,6 +18,14 @@ const setupInput = function() {
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
+  } else if (key === 'w') {
+    connection.write('Move: up');
+  } else if (key === 'a') {
+    connection.write('Move: left');
+  } else if (key === 's') {
+    connection.write('Move: down');
+  } else if (key === 'd') {
+    connection.write('Move: right');
   }
 };
 
--- play.js
+++ play.js
@@ -2,6 +2,6 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log('Connecting ...');
-connect();
+// connect();
 
-setupInput();
+setupInput(connect());
